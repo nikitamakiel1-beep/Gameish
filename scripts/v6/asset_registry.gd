eef91067506cccf0f2718630994ff5884b386dd0
nikitamakiel1-beep <nikitamakiel1@gmail.com>
2026-08-06 10:27:26 +0200

    Route production assets through the bounded runtime registry

--- scripts/v6/asset_registry.gd
+++ scripts/v6/asset_registry.gd
@@ -1,4 +1,1 @@
-extends "res://scripts/v6/generated_asset_registry.gd"
-
-func validate_contract(deep: bool = false) -> Dictionary:
-	return super.validate_contract(deep)
+extends "res://scripts/v6/runtime_asset_registry.gd"
